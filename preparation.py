c2d1ee55530c0bd2abb048d40939fcc84bcad3c5
Caio Rear <c.d.rear@gmail.com>
2023-06-15 21:15:26 +0100

    commit

--- preparation.py
+++ preparation.py
@@ -1,85 +1,79 @@
 # imports----------------------------------------------------------------------
 import pandas as pd
 import numpy as np
+from tqdm.notebook import tqdm
 
 # add team performance---------------------------------------------------------
 
 
-class GeneData:
-    def __init__(self, data):
+def add_team_performance(data, window_size=38):
 
-        self.data = data
+    x_home_goals = data[data['was_home']].groupby(
+        ['fixture', 'team_h'], as_index=False)[['expected_goals']].sum()
+    x_away_goals = data[~data['was_home']].groupby(
+        ['fixture', 'team_a'], as_index=False)[['expected_goals']].sum()
 
-        self.def_widow
+    xG_table = x_away_goals.merge(
+        x_home_goals, on='fixture', suffixes=['_a', '_h'])
 
-        self.player_windows = []
-        self.next_fixture = []
+    x_home_goals = xG_table[[
+        'fixture', 'team_h', 'expected_goals_h', 'expected_goals_a']].rename(
+        columns={
+            'team_h': 'team',
+            'expected_goals_h': 'xG', 'expected_goals_a': 'xGA'})
 
-    # add xG and xGA for teams from recent games-------------------------------
-    def add_team_performance(self, window_size=38):
-
-        x_home_goals = self.data[self.data['was_home']].groupby(
-            ['fixture', 'team_h'], as_index=False)[['expected_goals']].sum()
-        x_away_goals = self.data[~self.data['was_home']].groupby(
-            ['fixture', 'team_a'], as_index=False)[['expected_goals']].sum()
-
-        xG_table = x_away_goals.merge(
-            x_home_goals, on='fixture', suffixes=['_a', '_h'])
-
-        x_home_goals = xG_table[[
-            'fixture', 'team_h',
-            'expected_goals_h', 'expected_goals_a']].rename(
+    x_away_goals = xG_table[[
+        'fixture', 'team_a', 'expected_goals_a', 'expected_goals_h']].rename(
             columns={
-                'team_h': 'team',
-                'expected_goals_h': 'xG', 'expected_goals_a': 'xGA'})
+                'team_a': 'team',
+                'expected_goals_a': 'xG', 'expected_goals_h': 'xGA'})
 
-        x_away_goals = xG_table[[
-            'fixture', 'team_a',
-            'expected_goals_a', 'expected_goals_h']].rename(
-                columns={
-                    'team_a': 'team',
-                    'expected_goals_a': 'xG', 'expected_goals_h': 'xGA'})
+    xG_table = pd.concat([x_home_goals, x_away_goals])
 
-        xG_table = pd.concat([x_home_goals, x_away_goals])
+    xG_cumulative = xG_table.groupby(['team', 'fixture']).sum().groupby(
+        level=0).rolling(window_size, min_periods=1).mean(
+        ).droplevel(0).reset_index()
 
-        xG_cumulative = xG_table.groupby(['team', 'fixture']).sum().groupby(
-            level=0).rolling(window_size, min_periods=1).mean(
-            ).droplevel(0).reset_index()
+    xG_cumulative = xG_cumulative[['fixture']].join(
+        xG_cumulative.sort_values('fixture').groupby(
+            'team').shift(1)[['xG', 'xGA']]).dropna(how='any')
 
-        xG_cumulative = xG_cumulative[['fixture']].join(
-            xG_cumulative.sort_values('fixture').groupby(
-                'team').shift(1)[['xG', 'xGA']]).dropna(how='any')
+    return data.merge(xG_cumulative.drop_duplicates('fixture').merge(
+        xG_cumulative.drop_duplicates('fixture', keep='last'),
+        on='fixture', suffixes=['_h', '_a']), on='fixture')
 
-        self.data = self.data.merge(xG_cumulative.drop_duplicates(
-            'fixture').merge(xG_cumulative.drop_duplicates(
-                'fixture', keep='last'),
-            on='fixture', suffixes=['_h', '_a']), on='fixture')
+# data windowing---------------------------------------------------------------
 
-    # add xG and xGA for teams from recent games-------------------------------
-    window_size = 3
 
-    def get_window_arrays(self, window_size=3):
+def get_window_arrays(data, window_size=3):
 
-        X, y = [], []
-        for player in tqdm(data['element'].unique(), desc='creating frames'):
+    batches = []
+    for player in tqdm(data['element'].unique(), desc='creating frames'):
 
-            player_data = data[
-                data['element'] == player].drop('element', axis=1).reset_index(
-                drop=True).copy()
+        player_data = data[
+            data['element'] == player].drop('element', axis=1).reset_index(
+            drop=True).copy()
 
-            frame_generator = player_data.drop(
-                player_data.tail(1).index).rolling(window=window_size)
+        frame_generator = player_data.drop(
+            player_data.tail(1).index).rolling(window=window_size)
 
-            for window in frame_generator:
+        for window in frame_generator:
 
-                if len(window) >= window_size:
-                    X.append(np.array(window, float))
+            if len(window) >= window_size:
 
-                    points_index = window.index[-1]
+                points_index = window.index[-1]
 
-                    y.append(player_data['total_points'].loc[points_index + 1])
+                match_data = np.array(player_data[[
+                    'was_home', 'team_a_difficulty',
+                    'team_h_difficulty', 'time_diffs']].loc[points_index + 1])
 
-        return np.array(X), np.array(y, float)
+                target_data = player_data['total_points'].loc[points_index + 1]
 
-    X_train, y_train = get_window_arrays(train_data)
-    X_test, y_test = get_window_arrays(test_data)
+                batch = {
+                    'player_data': np.array(window, float),
+                    'match_data': match_data,
+                    'target_data': target_data}
+
+                batches.append(batch)
+
+    return batches
--- windowing.py
+++ windowing.py
@@ -1,37 +1,101 @@
 
-# test-train split-------------------------------------------------------------
-# window_size = 3
+# train-validate-test split----------------------------------------------------
 
-# cutoff = 30
 
-# train_data = data[data['round'] < cutoff].drop('round', axis=1)
-# test_data = data[data['round'] >= cutoff].drop('round', axis=1)
 
-# def get_window_arrays(data):
 
-#     X, y = [], []
-#     for player in tqdm(data['element'].unique(), desc='creating frames'):
 
-#         player_data = data[
-#             data['element'] == player].drop('element', axis=1).copy()
 
-#         frame_generator = player_data.drop(
-#             player_data.tail(1).index).rolling(window=window_size)
 
-#         for window in frame_generator:
 
-#             if len(window) >= 3:
-#                 X.append(np.array(window, float))
 
-#                 points_index = window.index[-1]
-#                 y.append(player_data['total_points'].loc[points_index + 1])
 
-#     return np.array(X), np.array(y, float)
 
-# X_train, y_train = get_window_arrays(train_data)
-# X_test, y_test = get_window_arrays(test_data)
 
-# np.save('data/X_train', X_train)
-# np.save('data/y_train', y_train)
-# np.save('data/X_test', X_test)
-# np.save('data/y_test', y_test)
+
+
+# class GeneData:
+#     def __init__(self, data):
+
+#         self.data = data
+
+#     def prepare(
+#             self, fixture_window_size, position,
+#             player_window_size, ttv_split=[0.7,0.2,0.1]):
+
+#         self.add_team_performance(window_size=fixture_window_size)
+
+#         self.ttv_split(splits=ttv_split)
+
+#         self.
+
+#         self.
+
+#     # add xG and xGA for teams from recent games-------------------------------
+#     def add_team_performance(self, window_size=38):
+
+#         x_home_goals = self.data[self.data['was_home']].groupby(
+#             ['fixture', 'team_h'], as_index=False)[['expected_goals']].sum()
+#         x_away_goals = self.data[~self.data['was_home']].groupby(
+#             ['fixture', 'team_a'], as_index=False)[['expected_goals']].sum()
+
+#         xG_table = x_away_goals.merge(
+#             x_home_goals, on='fixture', suffixes=['_a', '_h'])
+
+#         x_home_goals = xG_table[[
+#             'fixture', 'team_h',
+#             'expected_goals_h', 'expected_goals_a']].rename(
+#             columns={
+#                 'team_h': 'team',
+#                 'expected_goals_h': 'xG', 'expected_goals_a': 'xGA'})
+
+#         x_away_goals = xG_table[[
+#             'fixture', 'team_a',
+#             'expected_goals_a', 'expected_goals_h']].rename(
+#                 columns={
+#                     'team_a': 'team',
+#                     'expected_goals_a': 'xG', 'expected_goals_h': 'xGA'})
+
+#         xG_table = pd.concat([x_home_goals, x_away_goals])
+
+#         xG_cumulative = xG_table.groupby(['team', 'fixture']).sum().groupby(
+#             level=0).rolling(window_size, min_periods=1).mean(
+#             ).droplevel(0).reset_index()
+
+#         xG_cumulative = xG_cumulative[['fixture']].join(
+#             xG_cumulative.sort_values('fixture').groupby(
+#                 'team').shift(1)[['xG', 'xGA']]).dropna(how='any')
+
+#         self.data = self.data.merge(xG_cumulative.drop_duplicates(
+#             'fixture').merge(xG_cumulative.drop_duplicates(
+#                 'fixture', keep='last'),
+#             on='fixture', suffixes=['_h', '_a']), on='fixture')
+
+#     # add xG and xGA for teams from recent games-------------------------------
+#     window_size = 3
+
+#     def get_window_arrays(self, window_size=3):
+
+#         X, y = [], []
+#         for player in tqdm(data['element'].unique(), desc='creating frames'):
+
+#             player_data = data[
+#                 data['element'] == player].drop('element', axis=1).reset_index(
+#                 drop=True).copy()
+
+#             frame_generator = player_data.drop(
+#                 player_data.tail(1).index).rolling(window=window_size)
+
+#             for window in frame_generator:
+
+#                 if len(window) >= window_size:
+#                     X.append(np.array(window, float))
+
+#                     points_index = window.index[-1]
+
+#                     y.append(player_data['total_points'].loc[points_index + 1])
+
+#         return np.array(X), np.array(y, float)
+
+#     X_train, y_train = get_window_arrays(train_data)
+#     X_test, y_test = get_window_arrays(test_data)
